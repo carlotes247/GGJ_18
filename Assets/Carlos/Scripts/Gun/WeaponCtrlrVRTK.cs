4fdd0093443c9af5c2cf77c0ff3a560d793c3a09
Carlotes_Laptop <carlotes247@gmail.com>
2018-01-27 23:51:05 +0000

    WE HAVE A GUN IN VR YAAAY

---
+++ Assets/Carlos/Scripts/Gun/WeaponCtrlrVRTK.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+/// <summary>
+/// The Weapon Controller interface that will make the gun work with VRTK
+/// </summary>
+public class WeaponCtrlrVRTK : VRTK_InteractableObject {
+
+    public bool Using;
+
+    public override void StartUsing(VRTK_InteractUse usingObject)
+    {
+        base.StartUsing(usingObject);
+        Using = true;
+    }
+
+    public override void StopUsing(VRTK_InteractUse usingObject)
+    {
+        base.StopUsing(usingObject);
+        Using = false;
+    }
+
+    public override void UpdatePass()
+    {
+        if (Using)
+        {
+            GameManager.Instance.Weapon.ShootFromGun();
+        }
+
+        Debug.Log("I'm being used!!");
+    }
+
+
+}
