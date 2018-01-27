aa8fe71b3f4fe786ff5ac72324b43ed21379ca24
murray85 <federicoliva@gmail.com>
2018-01-27 14:44:14 +0000

    Fed AudioManager Update 1

---
+++ Assets/Fed/AudioManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public void PlaySugarPickUp()
+    }
+        SugarPickUp.Play();
+
+    }
+
+
+    public void PlaySugarRelease()
+    {
+        SugarRelease.Play();
+
+    }
+    
+
+    public void PlayBacteriaChew()
+    {
+        BacteriaChew.Play();
+
+    }
+
+
+    public void PlayBacteriaHappy()
+    {
+        BacteriaHappy.Play();
+
+    }
+
+
+    public void PlayBacteriaGrowth()
+    {
+        BacteriaGrowth.Play();
+
+    }
+
+
+    public void PlayBacteriaShake()
+    {
+        BacteriaShake.Play();
+
+    }
+
+
+    public void PlayBacteriaShrink()
+    {
+        BacteriaShrink.Play();
+
+    }
+
+
+    public void PlayBacteriaSad()
+    {
+        BacteriaSad.Play();
+
+    }
+
+
+    public void PlayCharacterSwallow()
+    {
+        CharacterSwallow.Play();
+
+    }
+
+
+    public void PlayCharacterHeal()
+    {
+        CharacterHeal.Play();
+
+    }
+
+
+    public void PlayCharacterDeath()
+    {
+        CharacterDeath.Play();
+
+    }
+
+
+    public void PlayCharacterDamage()
+    {
+        CharacterDamage.Play();
+
+    }
