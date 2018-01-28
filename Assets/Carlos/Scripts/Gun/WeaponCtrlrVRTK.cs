667f4113190f80d096545e4042ca14195b3175d3
Carlotes_Laptop <carlotes247@gmail.com>
2018-01-28 14:27:37 +0000

    WE HAVE AN ICE GUN!

--- Assets/Carlos/Scripts/Gun/WeaponCtrlrVRTK.cs
+++ Assets/Carlos/Scripts/Gun/WeaponCtrlrVRTK.cs
@@ -9,6 +9,20 @@ using VRTK;
 public class WeaponCtrlrVRTK : VRTK_InteractableObject {
 
     public bool Using;
+    [SerializeField]
+    private WeaponController m_Weapon;
+
+    private void Start()
+    {
+        if (m_Weapon == null)
+        {
+            this.GetComponent<WeaponController>();
+            if (m_Weapon == null)
+            {
+                Debug.LogError("Weapon Ctrler not found in " + this.name);
+            }
+        }
+    }
 
     public override void StartUsing(VRTK_InteractUse usingObject)
     {
@@ -26,7 +40,7 @@ public class WeaponCtrlrVRTK : VRTK_InteractableObject {
     {
         if (Using)
         {
-            GameManager.Instance.Weapon.ShootFromGun();
+            m_Weapon.ShootFromGun();
         }
 
         Debug.Log("I'm being used!!");
